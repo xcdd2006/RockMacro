b017064c82d5d8fafd5b828d7283f8d493d0f421
qwq <xcdd2006@qq.com>
2026-07-08 10:56:56 +0800

    添加写入权限

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,11 @@
 pluginManagement {
+    val useAliyunRepo = System.getenv("USE_ALIYUN_REPO")?.toBoolean() ?: true
     repositories {
+        if (useAliyunRepo) {
+            maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
+            maven { setUrl("https://maven.aliyun.com/repository/google") }
+            maven { setUrl("https://maven.aliyun.com/repository/public") }
+        }
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -9,8 +15,13 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 dependencyResolutionManagement {
+    val useAliyunRepo = System.getenv("USE_ALIYUN_REPO")?.toBoolean() ?: true
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        if (useAliyunRepo) {
+            maven { setUrl("https://maven.aliyun.com/repository/google") }
+            maven { setUrl("https://maven.aliyun.com/repository/public") }
+        }
         google()
         mavenCentral()
     }
